5d449e9f096aaf8ad1ef4762863a1c235070fa05
Aayushi Varma <aayushiv@adobe.com>
2022-06-26 16:21:37 +0530

    reorganized Modal component

--- src/main/webpack/components/dialogmodal/doc/dialogmodal.stories.js
+++ src/main/webpack/components/dialogmodal/doc/dialogmodal.stories.js
@@ -1,14 +1,10 @@
-import ModalDialog from "./../modal/modal.hbs";
-import DatePickerDialog from "./../datepicker/datepicker.hbs";
+import ModalDialog from "../dialogmodal.hbs";
 
 export default {
-  title: "Components/Dialog (Modal)",
+  title: "Components/Modal/Dialog",
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {},
 };
 
 const TemplateModal = ({ label, ...args }) => ModalDialog(args);
-export const Modal = TemplateModal.bind({});
-
-const TemplateDatePicker = ({ label, ...args }) => DatePickerDialog(args);
-export const DatePicker = TemplateDatePicker.bind({});
+export const Dialog = TemplateModal.bind({});
