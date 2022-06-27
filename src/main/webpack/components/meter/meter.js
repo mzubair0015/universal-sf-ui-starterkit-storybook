1669ba7c926e55b82a49c39b6436ee9a4cae3dc7
Aayushi Varma <aayushiv@adobe.com>
2022-06-27 10:05:25 +0530

    reorganized Meter component

--- src/main/webpack/components/meter/meter.js
+++ src/main/webpack/components/meter/meter.js
@@ -1,12 +1,12 @@
-"use strict";
-var Meter = (element) => {
+'use strict';
+var Meter = function (element) {
   this.rootEl = element;
-  this.fillEl = element.querySelector(".fill");
+  this.fillEl = element.querySelector('.fill');
 
   // set up min, max, and current value
-  var min = element.getAttribute("aria-valuemin");
-  var max = element.getAttribute("aria-valuemax");
-  var value = element.getAttribute("aria-valuenow");
+  var min = element.getAttribute('aria-valuemin');
+  var max = element.getAttribute('aria-valuemax');
+  var value = element.getAttribute('aria-valuenow');
   this._update(parseFloat(min), parseFloat(max), parseFloat(value));
 };
 
@@ -15,9 +15,9 @@ var Meter = (element) => {
 // returns a number representing a percentage between 0 - 100
 Meter.prototype._calculatePercentFill = function (min, max, value) {
   if (
-    typeof min !== "number" ||
-    typeof max !== "number" ||
-    typeof value !== "number"
+    typeof min !== 'number' ||
+    typeof max !== 'number' ||
+    typeof value !== 'number'
   ) {
     return 0;
   }
@@ -31,7 +31,7 @@ Meter.prototype._getColorValue = function (percent) {
   // if percent is 100, hue should be red, and if percent is 0, hue should be green
   var hue = (percent / 100) * (0 - 120) + 120;
 
-  return "hsl(" + hue + ", 100%, 40%)";
+  return 'hsl(' + hue + ', 100%, 40%)';
 };
 
 // no return value; updates the meter element
@@ -39,24 +39,24 @@ Meter.prototype._update = function (min, max, value) {
   // update fill width
   if (min !== this.min || max !== this.max || value !== this.value) {
     var percentFill = this._calculatePercentFill(min, max, value);
-    this.fillEl.style.width = percentFill + "%";
+    this.fillEl.style.width = percentFill + '%';
     this.fillEl.style.color = this._getColorValue(percentFill);
   }
 
   // update aria attributes
   if (min !== this.min) {
     this.min = min;
-    this.rootEl.setAttribute("aria-valuemin", min + "");
+    this.rootEl.setAttribute('aria-valuemin', min + '');
   }
 
   if (max !== this.max) {
     this.max = max;
-    this.rootEl.setAttribute("aria-valuemax", max + "");
+    this.rootEl.setAttribute('aria-valuemax', max + '');
   }
 
   if (value !== this.value) {
     this.value = value;
-    this.rootEl.setAttribute("aria-valuenow", value + "");
+    this.rootEl.setAttribute('aria-valuenow', value + '');
   }
 };
 
@@ -64,7 +64,7 @@ Meter.prototype._update = function (min, max, value) {
 
 // no return value; modifies the meter element based on a new value
 Meter.prototype.setValue = function (value) {
-  if (typeof value !== "number") {
+  if (typeof value !== 'number') {
     value = parseFloat(value);
   }
 
@@ -75,9 +75,9 @@ Meter.prototype.setValue = function (value) {
 
 /* Code for example page */
 
-window.addEventListener("load", function () {
+window.addEventListener('load', function () {
   // init meters
-  var meterEls = document.querySelectorAll("[role=meter]");
+  var meterEls = document.querySelectorAll('[role=meter]');
   var meters = [];
   Array.prototype.slice.call(meterEls).forEach(function (meterEl) {
     meters.push(new Meter(meterEl));
@@ -98,18 +98,18 @@ window.addEventListener("load", function () {
   var updateInterval = playMeters();
 
   // play/pause meter updates
-  var playButton = document.querySelector(".play-meters");
-  playButton.addEventListener("click", function () {
-    var isPaused = playButton.classList.contains("paused");
+  var playButton = document.querySelector('.play-meters');
+  playButton.addEventListener('click', function () {
+    var isPaused = playButton.classList.contains('paused');
 
     if (isPaused) {
       updateInterval = playMeters();
-      playButton.classList.remove("paused");
-      playButton.innerHTML = "Pause Updates";
+      playButton.classList.remove('paused');
+      playButton.innerHTML = 'Pause Updates';
     } else {
       clearInterval(updateInterval);
-      playButton.classList.add("paused");
-      playButton.innerHTML = "Start Updates";
+      playButton.classList.add('paused');
+      playButton.innerHTML = 'Start Updates';
     }
   });
 });
