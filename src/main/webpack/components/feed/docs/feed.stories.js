8d967b6e04bccd09468cb96b37c50ec2417baba6
Pragati Singh <pragatis@adobe.com>
2022-07-06 01:00:17 +0530

    feed component updated

--- src/main/webpack/components/feed/docs/feed.stories.js
+++ src/main/webpack/components/feed/docs/feed.stories.js
@@ -1,12 +1,12 @@
-import AlertStandard from "../alert.hbs";
+import FeedStandard from "../feed.hbs";
 
 import "../../../site/main.scss";
 
 export default {
-  title: "Components/Alert",
+  title: "Components/Feed",
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {},
 };
 
-const TemplateStandard = ({ label, ...args }) => AlertStandard();
+const TemplateStandard = ({ label, ...args }) => FeedStandard();
 export const Standard = TemplateStandard.bind();
