0642c98d6f4835ea264052b70190c74a58f416e8
Pragati Singh <pragatis@adobe.com>
2022-07-06 01:10:10 +0530

    link component added

--- src/main/webpack/components/link/docs/link.stories.js
+++ src/main/webpack/components/link/docs/link.stories.js
@@ -1,12 +1,12 @@
-import FeedStandard from "../feed.hbs";
+import LinkStandard from "../link.hbs";
 
 import "../../../site/main.scss";
 
 export default {
-  title: "Components/Feed",
+  title: "Components/Link",
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {},
 };
 
-const TemplateStandard = ({ label, ...args }) => FeedStandard();
+const TemplateStandard = ({ label, ...args }) => LinkStandard();
 export const Standard = TemplateStandard.bind();
