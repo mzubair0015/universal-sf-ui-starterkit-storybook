0da0c50bb610fd19de590233ca528ec301feeaf8
Pragati Singh <pragatis@adobe.com>
2022-07-07 18:15:37 +0530

    added all variations

---
+++ src/main/webpack/components/menubutton-navigation/docs/menubutton-navigation.stories.js
@@ -0,0 +1,12 @@
+import MenuButtonNavigation from "../menubutton-navigation.hbs";
+
+import "../../../site/main.scss";
+
+export default {
+  title: "Components/MenubuttonNavigation",
+  // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
+  argTypes: {},
+};
+
+const TemplateNavigation = ({ label, ...args }) => MenuButtonNavigation();
+export const Navigation = TemplateNavigation.bind()
