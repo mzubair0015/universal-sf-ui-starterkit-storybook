163cdb09c94ddc6eeafd7850cf6f5ea82efc6f97
gsnair <gsnair@adobe.com>
2022-07-08 16:52:50 +0530

    updated code, split the variations to components

--- src/main/webpack/components/tabs-manual/docs/tabs.stories.js
+++ src/main/webpack/components/tabs-manual/docs/tabs.stories.js
@@ -1,14 +1,10 @@
-import TabsAuto from "../tabs-automatic.hbs";
 import TabsManual from "../tabs-manual.hbs";
 
 export default {
-  title: "Components/Tabs",
+  title: "Components/Tabs with Manual Activation",
   // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
   argTypes: {},
 };
-
-const TemplateTabsAuto = ({ label, ...args }) => TabsAuto(args);
-export const Automatic = TemplateTabsAuto.bind({});
 
 const TemplateTabsManual = ({ label, ...args }) => TabsManual(args);
 export const Manual = TemplateTabsManual.bind({});
