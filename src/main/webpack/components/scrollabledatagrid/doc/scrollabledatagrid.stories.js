6a79527c5183e0e8c8a68336e456eb699fdb8c72
Hanush H Nair <hanushh@gmail.com>
2022-09-26 22:29:57 +0530

    change folder structure

--- src/main/webpack/components/scrollabledatagrid/doc/scrollabledatagrid.stories.js
+++
@@ -1,9 +0,0 @@
-import ScrollableDataGrid from "../scrollabledatagrid.hbs";
-export default {
-  title: "Components/Grids/Data Grids/Scrollable",
-  // More on argTypes: https://storybook.js.org/docs/html/api/argtypes
-  argTypes: {},
-};
-
-const TemplateScrollable = ({ label, ...args }) => ScrollableDataGrid(args);
-export const Scrollable = TemplateScrollable.bind({});
