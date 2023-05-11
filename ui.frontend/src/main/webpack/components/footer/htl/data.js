8612cc21ffb2b6e7726b2fdbc8210ec4f2af7775
Subash Selvaraj <sesubash@gmail.com>
2023-05-11 18:37:15 +0530

    fixed htl tool issue in windows

--- ui.frontend/src/main/webpack/components/footer/htl/data.js
+++
@@ -1,9 +0,0 @@
-module.exports = class Data {
-    use() {
-      return {
-        "variation": "primary",
-        "copy": "&copy;2022 Adobe Systems Inc.",
-        "content": "Footer"
-      };
-    }
-  };
