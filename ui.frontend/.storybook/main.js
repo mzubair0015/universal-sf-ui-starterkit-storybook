fcec1fddc4108f873431639a47eba703108d2e6d
sagar-adobe <104062037+sagar-adobe@users.noreply.github.com>
2023-07-03 10:30:41 +0530

    Missing config for variables to work

--- ui.frontend/.storybook/main.js
+++ ui.frontend/.storybook/main.js
@@ -11,7 +11,12 @@ module.exports = {
     "@storybook/theming",
     "@storybook/addon-a11y",
   ],
-  staticDirs: ['../src/main/webpack/resources', '../src/main/webpack/static', '../static', '../src/main/webpack/core-components'],
+  staticDirs: [
+    "../src/main/webpack/resources",
+    "../src/main/webpack/static",
+    "../static",
+    "../src/main/webpack/core-components",
+  ],
   webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
       test: /\.handlebars|hbs$/,
@@ -50,10 +55,10 @@ module.exports = {
           loader: "sass-loader",
           options: {
             sourceMap: true,
-            additionalData: [
-              `$env: ${process.env.NODE_ENV}`,
-              `$resource-path: ${process.env.RESOURCE_LOCAL_PATH}`
-            ],
+            additionalData: `
+              $env: ${process.env.NODE_ENV};
+              $resource-path: ${process.env.RESOURCE_LOCAL_PATH};
+            `,
           },
         },
       ],
